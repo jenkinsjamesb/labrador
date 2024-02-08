e49e30e5bef9323647da4934c84b19878dec4c63
Brad <91520623+jenkinsjamesb@users.noreply.github.com>
2024-02-08 12:51:34 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo docker system prune -fa
-sudo docker compose -f ~/leadlab/$1/$1-compose.yml pull
-sudo docker compose -f ~/leadlab/$1/$1-compose.yml --env-file ~leadlab/.env up
+sudo docker compose -f ~/leadlab/$1/$1-compose.yml --env-file ~/leadlab/.env pull
+sudo docker compose -f ~/leadlab/$1/$1-compose.yml --env-file ~/leadlab/.env up
